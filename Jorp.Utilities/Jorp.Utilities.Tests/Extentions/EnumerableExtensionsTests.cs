69977ed18dc24a3c26f48009974c4cfc093de28d
Saman Payam <saman.payam@xnom.se>
2020-08-03 15:26:06 +0200

    initial checkin
    flow setup

---
+++ Jorp.Utilities/Jorp.Utilities.Tests/Extentions/EnumerableExtensionsTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Jorp.Utilities.Extentions;
+using NUnit.Framework;
+
+namespace Jorp.Utilities.Tests.Extentions
+{
+    [TestFixture]
+    public class EnumerableExtensionsTests
+    {
+        [Test]
+        public void BaseEnumerableExtensionsChunkTest()
+        {
+            var testList = Enumerable.Range(1, 10000).Select(_=> _);
+            var batchList = testList.Chunk(100);
+
+            Assert.That(batchList.Count(), Is.EqualTo(100), "batch size does not match");
+        }
+
+        [Test]
+        public void EnumerableExtensionsPartielChunkTest()
+        {
+            var testList = Enumerable.Range(1, 10000).Select(_ => _);
+            var batchList = testList.Chunk(350);
+
+            Assert.IsTrue(batchList.Any());
+            Assert.That(batchList.ToList()[28].Count(), Is.EqualTo(200), "last chunk should contain 200 items");
+            Assert.That(batchList.Count(), Is.EqualTo(29), "batch size does not match");
+        }
+
+        [Test]
+        public void EnumerableExtensionsParallelThreadingAsyncInvokeTest()
+        {
+            var counter = 0;
+            var list = Enumerable.Range(1, 10000);
+            IList<Exception> thredExceptions = new List<Exception>();
+
+            TestContext.WriteLine($"Start at: {DateTime.Now.ToLongTimeString()}");
+            list.ParallelThreadingInvoke(100, 10, ref thredExceptions, (data)=>  Action(data, ref counter));
+            TestContext.WriteLine($"End at: {DateTime.Now.ToLongTimeString()}");
+
+            Assert.That(counter, Is.GreaterThan(0));
+            Assert.That(thredExceptions.Count(), Is.LessThanOrEqualTo(0));
+        }
+
+
+        [Test]
+        public void EnumerableExtensionsParallelThreadingSyncInvokeTest()
+        {
+            var counter = 0;
+            var list = Enumerable.Range(1, 10000);
+            IList<Exception> thredExceptions = new List<Exception>();
+
+            TestContext.WriteLine($"Start at: {DateTime.Now.ToLongTimeString()}");
+            list.ParallelThreadingInvoke(100, 1, ref thredExceptions, (data) => Action(data, ref counter));
+            TestContext.WriteLine($"End at: {DateTime.Now.ToLongTimeString()}");
+
+            Assert.That(counter, Is.GreaterThan(0));
+            Assert.That(thredExceptions.Count(), Is.LessThanOrEqualTo(0));
+        }
+
+        [Test]
+        public void EnumerableExtensionsParallelThreadingSyncInvokeWithExceptionTest()
+        {
+            var counter = 0;
+            var list = Enumerable.Range(1, 10000);
+            IList<Exception> thredExceptions = new List<Exception>();
+
+            TestContext.WriteLine($"Start at: {DateTime.Now.ToLongTimeString()}");
+            list.ParallelThreadingInvoke(100, 1, ref thredExceptions, (data) =>
+            {
+                counter++;
+
+                if (counter > 10)
+                {
+                    throw new Exception("test");
+                }
+            });
+
+            TestContext.WriteLine($"End at: {DateTime.Now.ToLongTimeString()}");
+
+            Assert.That(counter, Is.GreaterThan(0));
+            Assert.That(thredExceptions.Count(), Is.GreaterThanOrEqualTo(1));
+        }
+
+
+        private void Action(IEnumerable<int> obj, ref int counter)
+        {
+            System.Threading.Thread.Sleep(100);
+            counter++;
+        }
+    }
+}
---
+++ Jorp.Utilities/Jorp.Utilities/Extentions/EnumerableExtensions.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Jorp.Utilities.Extentions
+{
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Split list into yield parts of chunks
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="chunksize"></param>
+        /// <returns></returns>
+        public static IEnumerable<IEnumerable<T>> Chunk<T>(this IEnumerable<T> source, int chunksize)
+        {
+            while (source.Any())
+            {
+                yield return source.Take(chunksize);
+                source = source.Skip(chunksize);
+            }
+        }
+
+
+        public static IEnumerable<T> Add<T>(this IEnumerable<T> e, T value)
+        {
+
+            foreach (var cur in e)
+                yield return cur;
+
+            yield return value;
+        }
+
+        /// <summary>
+        /// Invoke parallel threading processing
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="threadSize"></param>
+        /// <param name="maxDegreeOfParallelism"></param>
+        /// <param name="action"></param>
+        /// <param name="thredExceptions"></param>
+        public static void ParallelThreadingInvoke<T>(this IEnumerable<T> source, int? threadSize,
+            int? maxDegreeOfParallelism, ref IList<Exception> thredExceptions, Action<IEnumerable<T>> action) 
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+
+            var internalExceptions = new List<Exception>();
+            var cancellationTokenSource = new CancellationTokenSource();
+
+            var batches = source
+                .ToList()
+                .Chunk(threadSize ?? source.ToList().Count())
+                .Count();
+
+            var actions = new List<Action>(batches);
+
+            source
+                .Select(_ => _)
+                .ToList()
+                .Chunk(threadSize ?? source.ToList().Count())
+                .ToList()
+                .ForEach(batch =>
+                    actions.Add(() =>
+                    {
+                        try
+                        {
+                            action(batch);
+                        }
+                        catch (Exception e)
+                        {
+                            internalExceptions.Add(e);
+
+                            cancellationTokenSource.Cancel();
+                            throw;
+                        }
+                    }));
+
+            var parallelOptions = new ParallelOptions()
+            {
+                MaxDegreeOfParallelism = maxDegreeOfParallelism ?? 1,
+                CancellationToken = cancellationTokenSource.Token
+            };
+
+            try
+            {
+                Parallel.Invoke(parallelOptions, actions.ToArray());
+            }
+            catch (Exception e)
+            {
+                if (thredExceptions is null)
+                    throw;
+                thredExceptions.Add(e);
+            }
+        }
+    }
+}
