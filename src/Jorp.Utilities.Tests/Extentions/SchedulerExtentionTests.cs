7639770c9cacf00858c7daa2b463364565e8da32
Saman Payam <saman.payam@xnom.se>
2020-09-10 22:31:29 +0200

    remove schema extention and add pre test for scheduler extention

---
+++ src/Jorp.Utilities.Tests/Extentions/SchedulerExtentionTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jorp.Utilities.Tests.Extentions
+{
+    [TestFixture]
+    public class SchedulerExtentionTests
+    {
+        //TODO: not finished
+
+    }
+}
